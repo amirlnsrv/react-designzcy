446fbe995a19492905860ff5edf74adf70be5c27
amirlnsrv <amir.lansarov@gmail.com>
2023-04-09 15:57:52 +0600

    feat: changing the display of the Latest block, using the LatestData database and the map method

---
+++ src/components/Content/Latest/LatestData.js
@@ -0,0 +1,33 @@
+import img1 from "../../../assets/img/Latest/1.jpg";
+import img2 from "../../../assets/img/Latest/2.jpg";
+import img3 from "../../../assets/img/Latest/3.jpg";
+import img4 from "../../../assets/img/Latest/4.jpg";
+import img5 from "../../../assets/img/Latest/5.jpg";
+import img6 from "../../../assets/img/Latest/6.jpg";
+
+export const LatestData = [
+  {
+    title: "Business Consultant Landing Page",
+    img: img1,
+  },
+  {
+    title: "Food delivery Web Design",
+    img: img2,
+  },
+  {
+    title: "BusMessenger landing Page",
+    img: img3,
+  },
+  {
+    title: "Doctor’s Consultant Landing Page",
+    img: img4,
+  },
+  {
+    title: "e-Leraning Web Design",
+    img: img5,
+  },
+  {
+    title: "Job Finder landing Page",
+    img: img6,
+  },
+];
--- src/components/Content/Latest/index.js
+++ src/components/Content/Latest/index.js
@@ -3,12 +3,7 @@ import classes from "./Latest.module.scss";
 import { Title } from "../../common/Title";
 import { LatestCard } from "../../common/LatestCard";
 import { Button } from "../../common/Button";
-import img1 from "../../../assets/img/Latest/1.jpg";
-import img2 from "../../../assets/img/Latest/2.jpg";
-import img3 from "../../../assets/img/Latest/3.jpg";
-import img4 from "../../../assets/img/Latest/4.jpg";
-import img5 from "../../../assets/img/Latest/5.jpg";
-import img6 from "../../../assets/img/Latest/6.jpg";
+import { LatestData } from "./LatestData";
 
 export const Latest = () => {
   return (
@@ -16,12 +11,15 @@ export const Latest = () => {
       <Title title="Our Creative Latest Projects" />
       <div className={classes.container}>
         <div className={classes.latest__cards}>
-          <LatestCard title="Business Consultant Landing Page" src={img1} />
-          <LatestCard title="Food delivery Web Design" src={img2} />
-          <LatestCard title="BusMessenger landing Page" src={img3} />
-          <LatestCard title="Doctor’s Consultant Landing Page" src={img4} />
-          <LatestCard title="e-Leraning Web Design" src={img5} />
-          <LatestCard title="Job Finder landing Page" src={img6} />
+          {LatestData.map((LatestCardData, index) => {
+            return (
+              <LatestCard
+                key={index}
+                title={LatestCardData.title}
+                src={LatestCardData.img}
+              />
+            );
+          })}
         </div>
         <div className={classes.latest__btn}>
           <Button title="VIEW ALL PROJECTS" />
